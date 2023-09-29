14245818a2fb9f74c4604e96fd1ed95c15e53083
Eviernes52 <Eviernes52@gmail.com>
2023-09-29 16:49:44 -0700

    css edits working on flip

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import Card from './components/Card';
 
 const leagueChampions = [
-  { hint: 'This champion is a yordle known as a hero who can disappear at whim .', champion: 'Teemo', difficulty: 'Easy' },
+  { hint: 'This champion is a yordle known as a hero who can disappear at whim.', champion: 'Teemo', difficulty: 'Easy' },
   { hint: 'This champion wields two huge axes and is known for spinning wildly into battle, also has a loud brother.', champion: 'Darius', difficulty: 'Medium' },
   { hint: 'This champion is a highly skilled marksman, hard to play.', champion: 'Vayne', difficulty: 'Hard' },
   { hint: 'This champion is able to heal in a large circle, normally seen flying around.', champion: 'Janna', difficulty: 'Medium' }
