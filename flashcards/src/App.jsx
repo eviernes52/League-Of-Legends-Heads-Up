bb4b58f6a645c91fde1a5101422bbe261a5f4798
Eviernes52 <Eviernes52@gmail.com>
2023-09-29 17:08:12 -0700

    added to list

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -4,9 +4,15 @@ import Card from './components/Card';
 
 const leagueChampions = [
   { hint: 'This champion is a yordle known as a hero who can disappear at whim.', champion: 'Teemo', difficulty: 'Easy' },
-  { hint: 'This champion wields two huge axes and is known for spinning wildly into battle, also has a loud brother.', champion: 'Darius', difficulty: 'Medium' },
-  { hint: 'This champion is a highly skilled marksman, hard to play.', champion: 'Vayne', difficulty: 'Hard' },
+  { hint: 'This champion wields two huge axes and is known for spinning wildly into battle, also has a loud brother.', champion: 'Darius', difficulty: 'Easy' },
+  { hint: 'This champion is a highly skilled marksman who uses a crossbow, hard to play.', champion: 'Vayne', difficulty: 'Hard' },
   { hint: 'This champion is able to heal in a large circle, normally seen flying around.', champion: 'Janna', difficulty: 'Medium' }
+  { hint: 'This champion is a red haired swift assassin who excels at picking off vulnerable enemies.', champion: 'Katarina', difficulty: 'Easy' },
+  { hint: 'This champion is a tanky support known for their crowd control abilities and shields, loves the cold.', champion: 'Braum', difficulty: 'Hard' },
+  { hint: 'This champion is a powerful mage who can manipulate time and space.', champion: 'Zilean', difficulty: 'Easy' },
+  { hint: 'This champion is a tricky trickster who can create copies of themselves.', champion: 'Shaco', difficulty: 'Medium' },
+  { hint: 'This champion is an armored prince who charges into battle with a massive spear.', champion: 'JarvanIV', difficulty: 'Medium' },
+  { hint: 'This champion is a fearsome pirate who hunts for treasure and battles with dual pistols.', champion: 'Miss Fortune', difficulty: 'Easy' },
 ];
 
 function App() {
